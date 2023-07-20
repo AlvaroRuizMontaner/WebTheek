7c7178a87dc5384dab8989e863430d4773c8565c
Varojausz <zoezil_ifra@hotmail.com>
2023-07-20 23:07:17 +0200

    progresando accordion y collapse

--- containers/accordion-nav/accordion-nav.container.tsx
+++ containers/accordion-nav/accordion-nav.container.tsx
@@ -1,22 +1,23 @@
 import { Link } from "@/components/link";
+import { Collapse } from "../collapse/collapse.container";
 import styles from "./accordion-nav.module.scss"
 
-export const Accordion = (props: AccordionProps): JSX.Element => {
+export const AccordionNav = (props: AccordionNavProps): JSX.Element => {
     const {open, info} = props;
     const mapInfoTSX = info.map((item, index) => (
         <div className={styles.content} key={index}>
             <Link href={item.url}><span className={styles.content__text}>{item.name}</span></Link>
-            {item.icon && <span></span>}
+            {/* {item.icon && <span></span>} */}
         </div>
     ))
     return (
-        <section className={`${styles.container} ${open ? styles.open : undefined}`}>
+        <Collapse height="40vh" open={open}>
             {mapInfoTSX}
-        </section>
+        </Collapse>
     )
 }
 
-interface AccordionProps {
+interface AccordionNavProps {
     open: boolean;
     info: {
         name: string,
